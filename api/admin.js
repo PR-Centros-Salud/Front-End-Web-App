63ef8413d6b7ce9e7cb38dd32b3c971223718a51
MALVV <blvckmalv@gmail.com>
2022-11-09 21:51:29 -0400

    Added documentation for api/admin.js

--- api/admin.js
+++ api/admin.js
@@ -6,12 +6,17 @@ import {authFetch} from './user'
 
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*"
 
+// Function that will be used to get the admin from the API
 export const getAdminByIdApi = async (id, logout) => {
     try {
+        // Define the URL
         const url = `${BASE_PATH}/admin/get/${id}`
+        // Make the petition to the API
         const result = await authFetch(url, { method: "GET" }, logout)
+        // Return the result of the petition
         return result ? result : null
     } catch (e) {
+        // Return null if there is an error
         console.log(e)
         return null
     }

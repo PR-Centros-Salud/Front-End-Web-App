3bb4e4081826fe6f2466b71fabd5a2189d038982
MALVV <blvckmalv@gmail.com>
2022-11-10 20:43:40 -0400

    Added documentation for api/doctors.js

--- api/doctors.js
+++ api/doctors.js
@@ -5,11 +5,14 @@ import axios from "axios"
 import {authFetch} from "./user"
 
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*"
-
+// Function that will be used to get Doctors from the API
 export const getDoctorsApi = async (logout) => {
     try {
+        // Define the URL
         const url = `${BASE_PATH}/medicalPersonal/institution`
+        // Make the petition GET to the API
         const result = await authFetch(url, { method: "GET" }, logout)
+        // Return the result of the petition
         return result
     } catch (e) {
         console.log(e)

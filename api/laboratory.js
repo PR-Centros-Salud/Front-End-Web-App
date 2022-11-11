58277795523e79482fb4e07b0845533f929639b0
MALVV <blvckmalv@gmail.com>
2022-11-10 20:51:13 -0400

    Added documentation for api/config.js from function createLaboratoryServiceApi

--- api/laboratory.js
+++ api/laboratory.js
@@ -26,21 +26,23 @@ export const getLaboratorySpecialistsApi = async (id, logout) => {
         const url = `${BASE_PATH}/medicalPersonal/labspecialists`
         // Make the petition GET to the API
         const result = await authFetch(url, { method: "GET" }, logout)
-        
+
         return result ? result : null
     } catch (e) {
         console.log(e)
         return null
     }
 }
-
+// Function that will be used to create Laboratory Service from the API
 export const createLaboratoryServiceApi = async (values, logout) => {
     try {
+        // Define the URL
         const url = `${BASE_PATH}/institution/laboratory/create`
+        // Define the data to send
         const formData = new FormData()
         formData.append("laboratory_service_name", values.name.trim())
         formData.append("medical_personal_id", values.medId)
-        
+        // Make the petition POST to the API
         const result = await authFetch(url, { method: "POST", body: formData, headers: { "Content-Type": "application/json" } }, logout)
         console.log(result)
         return result ? result : null

5986f0f3e10d18df2d714eccfbf96615cc68c2fc
MALVV <blvckmalv@gmail.com>
2022-11-10 20:42:26 -0400

    Added documentation for api/config.js

--- api/config.js
+++ api/config.js
@@ -4,12 +4,16 @@ import FormData from "form-data"
 import axios from "axios"
 import {authFetch} from "./user"
 
-axios.defaults.headers["Access-Control-Allow-Origin"] = "*"
 
+axios.defaults.headers["Access-Control-Allow-Origin"] = "*"
+// Function that will be used to get Provinces from the API
 export const getProvincesApi = async (logout) => {
     try {
+        // Define the URL
         const url = `${BASE_PATH}/config/available-provinces`
+        // Make the petition to the API
         const result = await axios.get(url)
+        // Return the result of the petition
         return result ? result : null
     } catch (e) {
         console.log(e)
